6bb25b100590e704aa45dd35f20f6413e517ff04
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-10-27 20:09:26 +0530

    Added all files

---
+++ E-Commerce Website/ECommerceWebsite/shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("About Us")
+
+
+def contact(request):
+    return HttpResponse("Contact Us")
+
+
+def track(request):
+    return HttpResponse("Track Order")
+
+
+def search(request):
+    return HttpResponse("Search Order")
+
+
+def productview(request):
+    return HttpResponse("View Product")
+
+
+def checkout(request):
+    return HttpResponse("Checkout")
