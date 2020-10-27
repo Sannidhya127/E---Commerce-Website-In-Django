578d26e76c4367a0822885b7b9c482d1b345e386
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-10-27 20:49:55 +0530

    Added the media directory to shops for loading images in a reccomended way

--- E-Commerce Website/ECommerceWebsite/ECommerceWebsite/urls.py
+++ E-Commerce Website/ECommerceWebsite/ECommerceWebsite/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='Home'),
     path('shop/', include('shop.urls')),
     path('blog/', include('Blog.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
